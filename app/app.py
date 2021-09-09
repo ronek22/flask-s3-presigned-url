7b04080b73ba4ebaab0f26fe3174a7f26e593d3e
ronek22 <jronek3010@gmail.com>
2021-09-09 14:54:53 +0200

    Add s3 object view

--- app/app.py
+++ app/app.py
@@ -6,9 +6,11 @@ from flask import Flask, jsonify, request, render_template, flash, redirect, url
 import requests
 import os
 import logging
+import boto3
 from uuid import uuid4
+from botocore.client import Config
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.ERROR,
                    format='[%(asctime)s]: {} %(levelname)s %(message)s'.format(os.getpid()),
                    datefmt='%Y-%m-%d %H:%M:%S',
                    handlers=[logging.StreamHandler()])
@@ -25,6 +27,19 @@ def create_app():
     class FileForm(FlaskForm):
         file = FileField("File")
 
+    def get_s3_objects():
+        session = boto3.Session(
+            aws_access_key_id=config.AWS_ID_KEY,
+            aws_secret_access_key=config.AWS_SECRET_KEY,
+            region_name='eu-central-1'
+        )
+        s3 = session.resource('s3')
+        bucket = s3.Bucket(config.AWS_S3_BUCKET)
+        bucket_list = bucket.objects.all()
+        s3_client = session.client('s3', config=Config(signature_version='s3v4'))
+        bucket_with_urls = [(obj, s3_client.generate_presigned_url('get_object', Params={'Bucket': config.AWS_S3_BUCKET, 'Key': obj.key}, ExpiresIn=60)) for obj in bucket_list]
+        return bucket_with_urls
+
     @app.get("/s3/health")
     def health():
         return {"health": "ok"}, 200
@@ -33,7 +48,8 @@ def create_app():
     @app.get("/s3")
     def index():
         form = FileForm()
-        return render_template("form.html", form=form)
+        object_list = get_s3_objects()
+        return render_template("form.html", form=form, object_list=object_list, bucket=config.AWS_S3_BUCKET)
 
     @app.post('/s3')
     def upload_file():
@@ -44,7 +60,7 @@ def create_app():
         else:
             return jsonify(error="requires file")
 
-        s3 = AwsS3UploadClass(app.config['AWS_ID_KEY'], app.config['AWS_SECRET_KEY'], app.config['AWS_S3_BUCKET'])
+        s3 = AwsS3UploadClass(config.AWS_ID_KEY, config.AWS_SECRET_KEY, config.AWS_S3_BUCKET)
         key = f"{str(uuid4())}_{file.filename}"
         file.save(key)
         response = s3.create_presigned_post(key)
